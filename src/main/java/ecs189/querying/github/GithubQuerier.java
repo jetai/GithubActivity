27fe4b77ab9f9f660137bb73b71f9ead46675520
Jeffrey <jeffrey@campus-097-151.ucdavis.edu>
2017-03-16 12:42:29 -0700

    no token

--- src/main/java/ecs189/querying/github/GithubQuerier.java
+++ src/main/java/ecs189/querying/github/GithubQuerier.java
@@ -43,25 +43,52 @@ public class GithubQuerier {
             String formatted = outFormat.format(date);
 
             // Add type of event as header
-            sb.append("<h3 class=\"type\">");
-            sb.append(type);
-            sb.append("</h3>");
-            // Add formatted date
-            sb.append(formatted);
+            sb.append("<div class=\"container\">");
+            sb.append("<div class=\"row\">");
+            sb.append("<div class=\"col-md-4\">");
+            sb.append("<h2>" + type + "</h2>");
+            sb.append("</div>");
+            sb.append("<div class=\"col-md-4\">");
+            sb.append("<h3 id=\"date\">" + formatted + "</h3>"); // Add formatted date
+            sb.append("</div>");
+            sb.append("</div>");
+            sb.append("</div>");
             sb.append("<br />");
+
             // Add SHA
-            sb.append("<b> SHA hash: </b>");
-            sb.append(SHA.substring(0,8));
+            sb.append("<div class=\"container\">");
+            sb.append("<table id=\"table\" class=\"table table-bordered\""); // begin table
+            sb.append("<thead>");
+            sb.append("<tr>");
+            sb.append("<th>SHA Hash</th>"); // Add SHA
+            sb.append("<th>Commit Message</th>"); // Add commit message
+            sb.append("<tr>");
+            sb.append("</thead>");
+            sb.append("<tbody>"); // begin tbody
+            sb.append("<tr>");
+            sb.append("<td>" + SHA.substring(0,8) + "</td>"); // SHA substring
+            sb.append("<td>" + message + "</td>"); // commit message
+            sb.append("</tr>");
+            sb.append("</tbody>"); // end tbody
+            sb.append("</table>"); // end table
+            sb.append("</div>");
+            //sb.append("<p>" + SHA.substring(0,8) + "</p>");
+            //sb.append("</div>");
             sb.append("<br />");
             // Add commit message
-            sb.append("<b> Commit message:</b> ");
-            sb.append(message);
-            sb.append("<br />");
+            //sb.append("<div class=\"col-sm-4\">");
+            //sb.append("<h3> Commit message: </h3> ");
+            //sb.append("<p>" + message + "</p>");
+            //sb.append("</div>");
+            //sb.append("</div>");
+            //sb.append("</div>");
+            //sb.append("<br />");
+
             // Add collapsible JSON textbox (don't worry about this for the homework; it's just a nice CSS thing I like)
             sb.append("<b> <a data-toggle=\"collapse\" href=\"#event-" + i + "\">JSON</a> </b>");
-            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\"> <pre>");
-            sb.append(event.toString());
-            sb.append("</pre> </div>");
+            sb.append("<div id=event-" + i + " class=\"collapse\" style=\"height: auto;\">");
+            sb.append("<pre>" + event.toString() + "</div>");
+            //sb.append("</pre> </div>");
         }
         sb.append("</div>");
         return sb.toString();
@@ -70,18 +97,38 @@ public class GithubQuerier {
     private static List<JSONObject> getEvents(String user) throws IOException {
         List<JSONObject> eventList = new ArrayList<JSONObject>();
         String access_token = "";
-        String url = BASE_URL + user + "/events" + access_token;
+        String url = BASE_URL + user + "/events?access_token=" + access_token;
         System.out.println(url);
-        JSONObject json = Util.queryAPI(new URL(url));
-        //System.out.println(json);
-        JSONArray events = json.getJSONArray("root");
-        for (int i = 0; i < events.length() && eventList.size() < 10; i++) {
-            JSONObject event = events.getJSONObject(i);
-            String type = event.getString("type");
-            if(type.equals("PushEvent")){
-                eventList.add(events.getJSONObject(i));
+
+        // Go through up to 50 pages
+        for (int j = 1; j < 50; j++) {
+            String newUrl = url + "&page=" + j;
+            JSONObject json = Util.queryAPI(new URL(newUrl));
+
+            if (eventList.size() == 10) break;
+
+            //System.out.println(json);
+            JSONArray events = json.getJSONArray("root");
+            for (int i = 0; i < events.length() && eventList.size() < 10; i++) {
+                JSONObject event = events.getJSONObject(i);
+                String type = event.getString("type");
+                if (type.equals("PushEvent")) {
+                    eventList.add(events.getJSONObject(i));
+                }
             }
         }
         return eventList;
+
+//        JSONObject json = Util.queryAPI(new URL(url));
+//        //System.out.println(json);
+//        JSONArray events = json.getJSONArray("root");
+//        for (int i = 0; i < events.length() && eventList.size() < 10; i++) {
+//            JSONObject event = events.getJSONObject(i);
+//            String type = event.getString("type");
+//            if(type.equals("PushEvent")){
+//                eventList.add(events.getJSONObject(i));
+//            }
+//        }
+//        return eventList;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,45 +8,52 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-    <link rel='stylesheet' href='webjars/bootstrap/3.2.0/css/bootstrap.min.css'>
+    <%--<meta name="viewport" content="initial-scale=1, maximum-scale=1">--%>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <%--<link rel='stylesheet' href='webjars/bootstrap/3.2.0/css/bootstrap.min.css'>--%>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel='stylesheet' href='main.css'>
     <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
     <title>Github Stalker</title>
   </head>
   <body>
-  <img src="https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/13495430_1297025703642731_4440836432524305740_o.jpg?oh=031ac3e1b1d6f8eaac8591a4a4b22de2&oe=59646A18">
-  <div id="core">
-    <h2>What Have I Been Up To?</h2>
-    <form action="index.jsp" method="GET">
-      <div class="form-group">
-        <label for="user"><h3>Github username:</h3></label>
-        <input type="user" name="user_name" class="form-control" id="user"
-               value="<%= request.getParameter("user_name") == null ? "" : request.getParameter("user_name")%>">
-      </div>
-      <%String user=request.getParameter("user_name"); %>
-      <button type="submit" class="btn btn-default">
-        <c:choose>
-          <c:when test="${empty user}">
-            Submit
-          </c:when>
-          <c:otherwise>
-            Refresh
-          </c:otherwise>
-        </c:choose>
-      </button>
-    </form>
-    <div id="activity">
-      <%if (user != null && !user.isEmpty()){%>
+  <div class="container">
+    <div class="jumbotron">
+      <div id="core">
+        <h2 id="title">What Have I Been Up To?</h2>
+        <form action="index.jsp" method="GET">
+          <div class="form-group">
+            <label for="user"><h3 id="title2">Github username:</h3></label>
+            <input type="user" name="user_name" class="form-control" id="user"
+                   value="<%= request.getParameter("user_name") == null ? "" : request.getParameter("user_name")%>">
+          </div>
+          <%String user=request.getParameter("user_name"); %>
+          <button type="submit" class="btn btn-default">
+            <c:choose>
+              <c:when test="${empty user}">
+                Submit
+              </c:when>
+              <c:otherwise>
+                Refresh
+              </c:otherwise>
+            </c:choose>
+          </button>
+        </form>
+        <div id="activity">
+        <%if (user != null && !user.isEmpty()){%>
           <%=GithubQuerier.eventsAsHTML(user)%>
         <% } else { %>
-            Tell me who you are, and I will tell you what you did last ... week? Month? Summer? Not sure yet.
+          <h4 id="tag">Tell me who you are, and I will tell you what you did last ... week? Month? Summer? Not sure yet.</h4>
         <% }%>
+        </div>
+      </div>
     </div>
   </div>
 
   <!-- YOUR CODE HERE -->
-  <script type="text/javascript" src="webjars/jquery/2.1.1/jquery.min.js"></script>
-  <script type="text/javascript" src="webjars/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+  <%--<script type="text/javascript" src="webjars/jquery/2.1.1/jquery.min.js"></script>--%>
+  <%--<script type="text/javascript" src="webjars/bootstrap/3.2.0/js/bootstrap.min.js"></script>--%>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </body>
 </html>
